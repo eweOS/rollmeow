eweOS/rollmeow

commit c02d704b7c18c0ffc4ede85ebb7176f8e138ed14

support retrying, follow location correctly

src/fetcher.lua
@@ -12,9 +12,12 @@ local table			= require "table";
 
 local cURL			= require "cURL";
 
+local rmHelpers			= require "helpers"
+
 local yield, easy	= coroutine.yield, cURL.easy;
 local insert, remove	= table.insert, table.remove;
 local concat		= table.concat;
+local verbosef		= rmHelpers.verbosef;
 
 local function
 fetcher(url)
@@ -27,6 +30,7 @@ fetcher(url)
 	end
 end
 
+-- TODO: make it configurable
 local function
 createHandleWithOpt(url)
 	return easy {
@@ -34,6 +38,7 @@ createHandleWithOpt(url)
 			[cURL.OPT_TIMEOUT]		= 10,
 			[cURL.OPT_LOW_SPEED_LIMIT]	= 10,
 			[cURL.OPT_LOW_SPEED_TIME]	= 10,
+			[cURL.OPT_FOLLOWLOCATION]	= true,
 		    };
 end
 
@@ -47,7 +52,7 @@ createConn(f, item)
 	end
 
 	local handle = createHandleWithOpt(url);
-	handle.data = { co = co, buf = {}, retry = 0 };
+	handle.data = { co = co, buf = {}, retry = 0, url = url };
 	return handle;
 end
 
@@ -64,6 +69,7 @@ nextConn(f, list)
 	return nil;
 end
 
+-- TODO: make retry configurable
 local function
 forEach(connections, f, originList)
 	local list = {};
@@ -86,9 +92,19 @@ forEach(connections, f, originList)
 		local newConn = false;
 
 		if type == "error" then
-			p.co(false, tostring(data));
-			newConn = true;
+			if p.retry < 3 then
+				p.retry = p.retry + 1;
+				verbosef("%s: sync failed, retry %d",
+					 p.url, p.retry);
+				handle = createHandleWithOpt(p.url);
+				handle.data = p;
+				multi:add_handle(handle);
+			else
+				p.co(false, tostring(data));
+				newConn = true;
+			end
 		elseif type == "done" then
+			insert(p.buf, data);
 			p.co(true, concat(p.buf));
 			newConn = true;
 		elseif type == "data" then

commit 6d6c13b55a92bb4dbba6ccfc7a2ac4ddf1d088a9

fetcher: Don't append data with type "done" to the received buffer
When dealing with HTTP protocol, the data with type "done" is actually
the status code instead of real data part that callers usually expect.
Drop it to avoid confusing the caller.

Fixes: c02d704b7c18 ("support retrying, follow location correctly")
Signed-off-by: Yao Zi <ziyao@disroot.org>

src/fetcher.lua
@@ -154,7 +154,6 @@ forEach(connections, f, originList)
 				newConn = true;
 			end
 		elseif type == "done" then
-			insert(p.buf, data);
 			p.co(true, concat(p.buf));
 			newConn = true;
 		elseif type == "data" then